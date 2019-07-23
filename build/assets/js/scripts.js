c9de2d53c4282315ba94863113b45bf2d21eb055
DemiGoT <kozachok_i@mail.ru>
2019-07-23 15:35:35 +0300

    Second changes

--- build/assets/js/scripts.js
+++ build/assets/js/scripts.js
@@ -24,27 +24,6 @@ function pageWidget(pages) {
 }
 
 
-// $('.header-slider').slick({
-//   infinite: true,
-//   dots: true,
-//   arrows: true,
-//   slidesToShow: 4,
-//   slidesToScroll: 4,
-//   responsive: [
-//     {
-//       breakpoint: 1024,
-//       settings: {
-//         slidesToShow: 3,
-//         slidesToScroll: 3,
-//         infinite: true,
-//         dots: true
-//       }
-//     }
-//   ]
-// });
-
-
-
 $('.section-block').slick({
     infinite: true,
     dots: false,
